3ec427341fe516e8edccecd41c3c57237334e8db
Nguyen  Hai <nguyenngochai006@gmail.com>
2021-02-16 08:42:48 +0700

    edit app

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Nguyen Ngoc Hai learn React Js
+          Hai
         </p>
 
       </header>
