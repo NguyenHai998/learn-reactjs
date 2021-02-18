395b4d713669d05f161b84bfd654bfec8623f9e5
Nguyen  Hai <nguyenngochai006@gmail.com>
2021-02-18 11:08:52 +0700

    info post fb

--- src/App.js
+++ src/App.js
@@ -2,16 +2,45 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const name = 'Hai';
+  const age = 23;
+  const isMale = true;
+  const info = {
+    university: 'Quang Binh University',
+    isLove: false
+  };
+  const colors = ['red', 'green', 'blue'];
+
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+
+        <p>My name {name}, {age} age</p>
         <p>
-          Lallalaaa
+          Gender:
+          {isMale && (
+            <>
+              <p>Male</p>
+              <p>Not Be De 😇 </p>
+            </>
+          )}
+          {!isMale && 'Female'}
         </p>
+        <p>Have studied at {info.university} and {info.isLove ? 'Co nguoi yeu roi' : 'Not Lover 😔'}</p>
+
+        <ul>
+          {colors.map(color => (
+            < li style={{ backgroundColor: color, listStyle: 'none' }}>{color}</li>
+          ))}
+        </ul>
+
+
 
       </header>
-    </div>
+    </div >
   );
 }
 
